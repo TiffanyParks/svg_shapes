d50984286e2e49924f1ad1ad17083a8de057968a
Tiffany <tapintobeauty08@gmail.com>
2023-05-05 08:32:52 -0400

    jest test

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const prompt = require('prompt');
-//inquirer to genterate questions
+const fetch = require('node-fetch');
+
 inquirer
+//   .prompt({
+//     type: 'input',
+//     name: 'username',
+//     message: 'Enter a Github Username:',
+//   })
+//   // promises are chained to directly pass inquirer data into fetch request
+//   .then((res) => fetch(`https://api.github.com/users/${res.username}`))
+//   // promises are chained to parse the request for the json data
+//   .then((res) => res.json())
+//   // json data is accepted as user and logged to the console
+//   .then((user) => console.log(user));
 
 
 // color, shape, and text for the logo
